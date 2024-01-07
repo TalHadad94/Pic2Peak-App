65965392bf3e424fd7708a0e7e5d26ba7b6eee27
Tal <talsmd95@gmail.com>
2024-01-08 00:28:10 +0200

    Adding size valriable

--- Canvas.py
+++ Canvas.py
@@ -10,16 +10,23 @@ path = '.\Pictures\logo.png'
 # Working:
 def Canvas(x, y):
     window = tk.Tk()
-    window.geometry('800x600')
+
+    # Frame:
+    size = str(x+10) + 'x' + str(y+55)
+    window.geometry(size)
     window.title('Pic2Peak')
 
+    # Buttons:
     button = tk.Button(window, text="Upload image", font=('Arial', 16), background='light grey')
     button.pack(pady=2)
 
+    # Workbox:
     canvas = tk.Canvas(window, width=x, height=y, bg='white')
     canvas.pack(anchor=tk.CENTER, expand=True)
 
+    # Picture:
     logo = tk.PhotoImage(file = path)
     canvas.create_image((x/2, y/2), image=logo)
+
 
     window.mainloop()
